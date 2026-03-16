ecdbec8b112d4fd484438341142bddcd1d1435e7
Manuel <manuclaro@gmail.com>
2026-03-16 18:26:19 -0300

    URL original

--- Comercio.NET.Mobile/Comercio.NET.Mobile.Server/Program.cs
+++ Comercio.NET.Mobile/Comercio.NET.Mobile.Server/Program.cs
@@ -1,53 +1,14 @@
 ﻿using Comercio.NET.Mobile.Server.Controllers;
 using Comercio.NET.Mobile.Server.Services;
-using System.Net;
-using System.Net.Sockets;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configurar puerto según el entorno
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
-// Handler IPv4 compartido para todos los HttpClients
-static SocketsHttpHandler CrearHandlerIPv4() => new SocketsHttpHandler
-{
-    ConnectCallback = async (context, cancellationToken) =>
-    {
-        var entries = await Dns.GetHostAddressesAsync(
-            context.DnsEndPoint.Host,
-            AddressFamily.InterNetwork,
-            cancellationToken);
-
-        if (entries.Length == 0)
-            throw new SocketException((int)SocketError.HostNotFound);
-
-        var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
-        {
-            NoDelay = true
-        };
-
-        try
-        {
-            await socket.ConnectAsync(entries[0], context.DnsEndPoint.Port, cancellationToken);
-            return new NetworkStream(socket, ownsSocket: true);
-        }
-        catch
-        {
-            socket.Dispose();
-            throw;
-        }
-    }
-};
-
-// Registrar HttpClient con nombre vacío (el que usan todos los servicios con CreateClient())
-builder.Services.AddHttpClient(string.Empty)
-    .ConfigurePrimaryHttpMessageHandler(CrearHandlerIPv4);
-
-// Agregar servicios
+builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 
-// Registrar servicios
 builder.Services.AddScoped<ArqueoCajaService>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<IProductosService, ProductosService>();
@@ -56,7 +17,6 @@ builder.Services.AddScoped<IVentasService, VentasService>();
 builder.Services.AddScoped<IAuditoriaService, AuditoriaService>();
 builder.Services.AddScoped<IMesasService, MesasService>();
 
-// CORS permisivo
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
